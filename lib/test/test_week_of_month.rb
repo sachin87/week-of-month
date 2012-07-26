684832435e233363619b7200e33e4d0cfd0203ab
Sachin Singh <singhsachin87@yahoo.com>
2012-07-27 00:32:15 +0530

    empty tests

---
+++ lib/test/test_week_of_month.rb
@@ -0,0 +1,32 @@
+require 'test/unit'
+require 'week_of_month'
+
+class HolaTest < Test::Unit::TestCase
+  def test_week_of_month
+    assert true
+  end
+
+  def test_week_split
+    
+  end
+  
+  def test_first_week?
+    
+  end
+
+  def test_second_week?
+    
+  end
+
+  def test_last_week?
+    
+  end
+
+  def test_total_weeks
+
+  end
+
+  def test_week_of_month_in_eng
+    
+  end
+end
