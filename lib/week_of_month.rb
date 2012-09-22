58f9452f2e6760c06c31b739e042a74d4431e555
Sachin Singh <singhsachin87@yahoo.com>
2012-09-22 21:28:55 +0530

    added support for non-ASCII chars

--- lib/week_of_month.rb
+++ lib/week_of_month.rb
@@ -1,23 +1,26 @@
+#!/bin/env ruby
+# encoding: utf-8
+
 require 'active_support'
 require 'active_support/core_ext/date/calculations.rb'
 
 class Date
 
-  WEEK_IN_ENG = {1 => "First", 2 => "Second",
-                3 => "Third", 4 => "Fourth",
-                5 => "Fifth", 6 => "Sixth"}
+  WEEK_IN_ENG = { 1 => 'First', 2 => 'Second',
+                  3 => 'Third', 4 => 'Fourth',
+                  5 => 'Fifth', 6 => 'Sixth' }
 
- # WEEK_IN_FR = {1 => "First", 2 => "Second",
-  #              3 => "Third", 4 => "Quatrième",
-  #              5 => "Cinquième", 6 => "sixième"}
-  # 
- # WEEK_IN_GER = {1 => "First", 2 => "Second",
-  #               3 => "Dritten", 4 => "Vierte",
-  #               5 => "Fünfte", 6 => "Sechste"}
+  WEEK_IN_FR = { 1 => 'First', 2 => 'Second',
+                 3 => 'Third', 4 => 'Quatrième',
+                 5 => 'Cinquième', 6 => 'sixième'}
+   
+  WEEK_IN_GER = { 1 => 'First', 2 => 'Second',
+                  3 => 'Dritten', 4 => 'Vierte',
+                  5 => 'Fünfte', 6 => 'Sechste'}
 
-#  WEEK_IN_JAP = {1=>"最初",2 =>"秒",
-#                3 =>"サード",4=> "第4回",
-#                5 =>"第五",6=> "シックス"}
+  WEEK_IN_JAP = { 1=>'最初', 2 =>'秒',
+                                    3 =>'サード', 4=> '第4回',
+                  5 =>'第五',6=> 'シックス' }
 
   def days_array
     day = self.beginning_of_month.wday
@@ -57,16 +60,16 @@ class Date
     WEEK_IN_ENG[week_of_month]
   end
 
-  #def week_of_month_in_fr
-  #  WEEK_IN_FR[week_of_month]
-  #end
+  def week_of_month_in_fr
+    WEEK_IN_FR[week_of_month]
+  end
 
-  #def week_of_month_in_ger
-  #  WEEK_IN_GER[week_of_month]
-  #end
+  def week_of_month_in_ger
+    WEEK_IN_GER[week_of_month]
+  end
 
-#  def week_of_month_in_jap
-#    WEEK_IN_JAP[week_of_month]
-#  end
+  def week_of_month_in_jap
+    WEEK_IN_JAP[week_of_month]
+  end
 
 end
