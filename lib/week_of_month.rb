a66ac67b17736332acc04ee64da333e60d0efd42
Sachin Singh <singhsachin87@yahoo.com>
2012-10-02 23:44:54 +0530

    test cases

--- lib/week_of_month.rb
+++ lib/week_of_month.rb
@@ -22,9 +22,9 @@ class Date
                   5 =>'第五',6=> 'シックス' }
 
   MONTH_WITH_DAY = { :january => 31, :february => 28, :march => 31, 
-                   :april => 30, :may => 31, :june => 30, :july => 31, 
-                   :august => 31, :september => 30, :october => 31, 
-                   :november => 30, :december => 31 }
+                     :april => 30, :may => 31, :june => 30, :july => 31, 
+                     :august => 31, :september => 30, :october => 31, 
+                     :november => 30, :december => 31 }
                    
   MONTH_WITH_SEQUENCE = { :january => 1, :february => 2, :march => 3, 
                           :april => 4, :may => 5, :june => 6, :july => 7, 
