b38177a574e459ef9139a82cbcb8f6636cf95657
Sachin87 <singhsachin87@yahoo.com>
2014-04-29 20:35:09 +0530

    code duplicacy  removed

--- lib/week_of_month.rb
+++ lib/week_of_month.rb
@@ -15,22 +15,30 @@ class Date
   include WeekOfMonth::Month
   include WeekOfMonth::Week
   include WeekOfMonth::Year
+end
 
-  unless method_defined?(:to_date)
-    def to_date
-      ::Date.new(year, month, day)
+[Time, Date].each do |klass|
+
+  klass.class_eval do
+
+    unless method_defined?(:to_date)
+      def to_date
+        ::Date.new(year, month, day)
+      end
     end
+
+    unless method_defined?(:sunday?)
+      def sunday?    ; self.wday == 0; end
+      def monday?    ; self.wday == 1; end
+      def tuesday?   ; self.wday == 2; end
+      def wednesday? ; self.wday == 3; end
+      def thursday?  ; self.wday == 4; end
+      def friday?    ; self.wday == 5; end
+      def saturday?  ; self.wday == 6; end
+    end
+
   end
 
-  unless method_defined?(:sunday?)
-    def sunday?    ; self.wday == 0; end
-    def monday?    ; self.wday == 1; end
-    def tuesday?   ; self.wday == 2; end
-    def wednesday? ; self.wday == 3; end
-    def thursday?  ; self.wday == 4; end
-    def friday?    ; self.wday == 5; end
-    def saturday?  ; self.wday == 6; end
-  end
 end
 
 class Time
@@ -47,21 +55,6 @@ class Time
     def self.new(year=Time.now.year, month=Time.now.month, day=Time.now.day, hour=Time.now.hour, min=Time.now.min, sec=Time.now.sec, millisecond=(Time.now.to_f * 1000.0).to_i)
       Time.local(year, month, day, hour, min, sec, millisecond)
     end
-  end
-
-  unless method_defined?(:to_date)
-    def to_date
-      ::Date.new(year, month, day)
-    end
-  end
-  unless method_defined?(:sunday?)
-    def sunday?    ; self.wday == 0; end
-    def monday?    ; self.wday == 1; end
-    def tuesday?   ; self.wday == 2; end
-    def wednesday? ; self.wday == 3; end
-    def thursday?  ; self.wday == 4; end
-    def friday?    ; self.wday == 5; end
-    def saturday?  ; self.wday == 6; end
   end
 end
 
